c82c5be2e5d7d56a9ba55cf03a5a241ca65ee2a3
kaziashraf947 <145238888+kaziashraf947@users.noreply.github.com>
2023-10-24 22:54:59 -0400

    Creation of components

---
+++ my-app/src/components/TicTacToe.jsx
@@ -0,0 +1,11 @@
+import Board from "./Board";
+
+function TicTacToe(){
+    return(
+        <div>
+            <h1>TicTacToe</h1>
+            <Board></Board>
+        </div>
+    )
+}
+export default TicTacToe;
